eea8efffde21aeb030630f13516f1d92872d1834
seongmok-kim <ksm1538@naver.com>
2025-11-16 20:58:32 +0900

    init

--- electron/main.js
+++ electron/main.js
@@ -1,20 +1,18 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: { nodeIntegration: true, contextIsolation: false }
-  })
+  });
 
   if (process.env.NODE_ENV === 'development') {
-    win.loadURL('http://localhost:5173') // 반드시 dev URL
+    win.loadURL('http://localhost:5173');
   } else {
-    win.loadFile(path.join(__dirname, '../frontend/dist/index.html')) // build 시
+    win.loadFile(path.join(__dirname, '../frontend/dist/index.html'));
   }
-
-  win.webContents.openDevTools() // 개발용
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
